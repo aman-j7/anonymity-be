a3fc88db73295708d7157c985d947244ebe23b14
Atma Gaurav <atmagaurav@Atmas-MacBook-Pro.local>
2026-03-14 16:00:33 +0530

    changes in GenerateRoomCode , added special character check

--- utils/roomcode.go
+++ utils/roomcode.go
@@ -3,19 +3,41 @@ package utils
 import "math/rand"
 
 const (
-	codeChars  = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
-	codeLength = 6
+	codeChars     = "!@#$%^&*ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+	specialChars  = "!@#$%^&*"
+	codeLength    = 6
 )
 
 func GenerateRoomCode(exists func(string) bool) string {
 	for {
 		code := make([]byte, codeLength)
+		hasSpecial := false
+
 		for i := range code {
-			code[i] = codeChars[rand.Intn(len(codeChars))]
+			c := codeChars[rand.Intn(len(codeChars))]
+			code[i] = c
+
+			if containsSpecial(c) {
+				hasSpecial = true
+			}
 		}
+
+		if !hasSpecial {
+			continue
+		}
+
 		s := string(code)
 		if !exists(s) {
 			return s
 		}
 	}
+}
+
+func containsSpecial(c byte) bool {
+	for i := 0; i < len(specialChars); i++ {
+		if c == specialChars[i] {
+			return true
+		}
+	}
+	return false
 }
