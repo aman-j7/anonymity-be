1422fbecf34fd3ad58bdd0992bd35fc3e532464f
Atma Gaurav <atmagaurav@Atmas-MacBook-Pro.local>
2026-03-14 16:06:10 +0530

    removed special character in room code generation

--- utils/roomcode.go
+++ utils/roomcode.go
@@ -3,41 +3,19 @@ package utils
 import "math/rand"
 
 const (
-	codeChars     = "!@#$%^&*ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
-	specialChars  = "!@#$%^&*"
-	codeLength    = 6
+	codeChars  = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+	codeLength = 6
 )
 
 func GenerateRoomCode(exists func(string) bool) string {
 	for {
 		code := make([]byte, codeLength)
-		hasSpecial := false
-
 		for i := range code {
-			c := codeChars[rand.Intn(len(codeChars))]
-			code[i] = c
-
-			if containsSpecial(c) {
-				hasSpecial = true
-			}
+			code[i] = codeChars[rand.Intn(len(codeChars))]
 		}
-
-		if !hasSpecial {
-			continue
-		}
-
 		s := string(code)
 		if !exists(s) {
 			return s
 		}
 	}
-}
-
-func containsSpecial(c byte) bool {
-	for i := 0; i < len(specialChars); i++ {
-		if c == specialChars[i] {
-			return true
-		}
-	}
-	return false
 }
